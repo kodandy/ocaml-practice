b4d5d3590f5a4be346bbab3f61fb19d041fe7310
Kazunari.NAKATA <o9o64456583@gmail.com>
2015-11-29 09:56:29 +0900

    fix

--- metro/station.ml
+++ metro/station.ml
@@ -1,8 +1,7 @@
- 
 (* 駅名データ *)
 type ekimei_t = {
     kanji   : string; (*漢字名*)
-    kana   : string; (*かな名*)
+    kana    : string; (*かな名*)
     romaji  : string; (*ローマ字名*)
     shozoku : string; (*所属路線名*)
 }
@@ -45,7 +44,6 @@ let hyoujiTest =
 
 
 (* ------------------- *)
-
 (* 問題10.10 *)
 (* 目的：駅名（文字列）と駅名リスト（ekimei_t list型）を受け取り、駅名を漢字表記で返す *)
 (* romaji_to_kanji : string -> ekimei_t list -> string *)
@@ -57,14 +55,15 @@ let rec romaji_to_kanji station_name global_ekimei_list = match global_ekimei_li
       else romaji_to_kanji station_name rest
 
 (* テスト *)
-let test1 romaji_to_kanji "akasaka" global_ekimei_list = "赤坂"
-let test2 romaji_to_kanji "ginza" global_ekimei_list = "銀座"
-let test3 romaji_to_kanji "ueno" global_ekimei_list = "上野"
+let test1 = romaji_to_kanji "akasaka" global_ekimei_list = "赤坂"
+let test2 = romaji_to_kanji "ginza" global_ekimei_list = "銀座"
+let test3 = romaji_to_kanji "ueno" global_ekimei_list = "上野"
 
 (* ------------------- *)
 (* 問題10.11 *)
 (* 目的：漢字の駅名ふたつ（文字列）と駅間リスト（ekikan_t list型）を受け取ったら駅間リストの中から2駅間の距離を返す *)
 (* romaji_to_kanji : string -> string -> ekikan_t list -> string *)
+
 (* let rec get_ekikan_kyori station_now station_next global_ekikan_list = match  *)
 
 
